104ebc8057be2ed8ee02c2d3618a533f26f705cb
Paweł Lidwin <lidwinpawel@gmail.com>
2026-01-17 12:51:44 +0100

    demo: add basic UI showing intialization of the SDK

--- demo/scripts/GalaxyManager.gd
+++ demo/scripts/GalaxyManager.gd
@@ -1,28 +1,61 @@
-extends Node
+extends Control
 
-func on_auth_success():
-	print("Success")
-	GDGalaxy.RequestUserStatsAndAchievements()
-	
-	
-func on_auth_failure(reason: GDGalaxy.AuthFailureReason):
-	print("Failed ", reason)
-	
-func on_auth_lost():
-	print("Auth lost")
-	
-func on_stats(userId: int):
-	print("Stats received ", userId)
+@onready var sign_in_status: Label = %"SignIn status"
+@onready var init_status: Label = %"Init status"
 
-func _init():
-	GDGalaxy.Init()
+@onready var init_button: Button = %"Init Button"
+@onready var sign_in_galaxy: Button = %SignInGalaxy
+@onready var logged_on_status: Label = %"LoggedOn status"
+
+var initialized = false
+
+func _ready():
+	init_button.pressed.connect(init_pressed)
+	sign_in_galaxy.pressed.connect(sign_in_galaxy_pressed)
 	
 	GDGalaxy.on_auth_success.connect(on_auth_success)
 	GDGalaxy.on_auth_failure.connect(on_auth_failure)
 	GDGalaxy.on_auth_lost.connect(on_auth_lost)
 	GDGalaxy.on_user_stats_and_achievements_retrieve_success.connect(on_stats)
-	
-	GDGalaxy.SignInGalaxy()
 
 func _process(_delta: float):
 	GDGalaxy.ProcessData()
+	
+func init_pressed():
+	if initialized: return
+	var success = GDGalaxy.Init()
+	initialized = success;
+	init_status.text = "Init: " + yes_or_no(success)
+	
+func sign_in_galaxy_pressed():
+	GDGalaxy.SignInGalaxy()
+
+func on_auth_failure(reason: GDGalaxy.AuthFailureReason):
+	var map = ['UNDEFINED',
+'GALAXY_SERVICE_NOT_AVAILABLE',
+'GALAXY_SERVICE_NOT_SIGNED_IN',
+'CONNECTION_FAILURE',
+'NO_LICENSE',
+'INVALID_CREDENTIALS',
+'GALAXY_NOT_INITIALIZED',
+'EXTERNAL_SERVICE_FAILURE',
+]
+	print('Auth failed ', reason)
+	sign_in_status.text = 'SignedIn: '  + yes_or_no(GDGalaxy.SignedIn()) + ' ' + map[reason]
+	logged_on_status.text = 'IsLoggedOn: ' + yes_or_no(GDGalaxy.IsLoggedOn())
+
+func on_auth_lost():
+	print('Auth lost')
+	sign_in_status.text = 'SignedIn: ' + yes_or_no(GDGalaxy.SignedIn()) + ' (AuthLost)'
+	logged_on_status.text = 'IsLoggedOn: ' + yes_or_no(GDGalaxy.IsLoggedOn()) + ' (AuthLost)'
+
+func on_auth_success():
+	print('Successful login')
+	sign_in_status.text = 'SignedIn: ' + yes_or_no(GDGalaxy.SignedIn())
+	logged_on_status.text = 'IsLoggedOn: ' + yes_or_no(GDGalaxy.IsLoggedOn())
+	
+func on_stats(userId: int):
+	print("Stats received ", userId)
+	
+func yes_or_no(val):
+	return "Yes" if val else "No"
